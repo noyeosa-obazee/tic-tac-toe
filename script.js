200cdf28390e83726e634a1953c87ff63f0b65f6
noyeosa-obazee <obazeenoyeosa@gmail.com>
2025-11-17 21:37:47 +0100

    Create function for computer turn

--- script.js
+++ script.js
@@ -1,8 +1,60 @@
-let example = [['x', 'o', 'x'], 
-               ['x', 'x', 'o'],
-               ['o', 'x', 'x']]
+const gameSquares = [...document.querySelectorAll('.game-square')]
+const domGameBoard = document.querySelector('.game-board')
+const xButton = document.querySelector('.x-button')
+const oButton = document.querySelector('.o-button')
+const pathChooser = document.querySelector('.button-arranger')
 
-function game (gameboard) {
+let chosenPath = 'x';
+let ticTacArr = [[], 
+                 [],
+                 []]
+let holdingArr = []
+let isComputerTurn = false
+
+xButton.addEventListener('click', function() {
+    chosenPath = 'x'
+    playGame()
+})
+
+oButton.addEventListener('click', function() {
+    chosenPath = 'o'
+    playGame()
+})
+
+function playGame() {
+for (let i = 0; i < gameSquares.length; i++) {
+        const square = gameSquares[i]
+        square.addEventListener('click', function() {
+
+            if(!gameSquares.every((s) => s.textContent.length > 0) && !isComputerTurn) {
+                holdingArr[i] = chosenPath
+                isComputerTurn = true
+                setTimeout(computerPlay, 3000);
+                
+
+            }
+             console.log(holdingArr)
+        })
+       
+    }
+}
+
+function computerPlay() {
+    const possibleIndexes = [0,1,2,3,4,5,6,7,8]
+    const takenIndexes = []
+for(let i = 0; i < holdingArr.length; i++) {
+    if(i in holdingArr) takenIndexes.push(i)
+}
+
+const notTakenIndexes = possibleIndexes.filter(e => !takenIndexes.includes(e))
+
+const randomIndex = notTakenIndexes[(Math.floor(Math.random() * notTakenIndexes.length))];
+holdingArr[randomIndex] = 'o';
+isComputerTurn = false
+console.log(holdingArr)
+}
+
+function getGameResult(gameboard) {
     for(const arr of gameboard) {
         if(arr.every(e => e === arr[0])) return `${arr[0]} wins!`
     }
@@ -15,5 +67,3 @@ function game (gameboard) {
     else if (arr1[2] === arr2[1] && arr2[1] === arr3[0]) return `${arr1[2]} wins!`
     else return 'We have a tie!';
 }
-
-console.log(game(example))
